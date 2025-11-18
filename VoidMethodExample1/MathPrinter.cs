40cd698a47c58e0a8c89b3d2fede9a42ec907910
solicyrus <cyrusamiri99@gmail.com>
2025-11-18 14:25:15 -0800

    Add files via upload

---
+++ VoidMethodExample1/MathPrinter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VoidMethodExample
+{
+    // This class contains a void method that takes two integers
+    public class MathPrinter
+    {
+        // Void method that does a math operation on the first number
+        // and prints the second number
+        public void DoMathAndPrint(int number1, int number2)
+        {
+            // Multiply the first number by 2
+            int result = number1 * 2;
+
+            // Display the result of the first number
+            Console.WriteLine("First number multiplied by 2: " + result);
+
+            // Display the second number
+            Console.WriteLine("Second number: " + number2);
+        }
+    }
+}
