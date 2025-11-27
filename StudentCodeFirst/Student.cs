536a29c726c69dad563e60a2c3b4f7d17a0062e8
solicyrus <cyrusamiri99@gmail.com>
2025-11-27 00:28:34 -0800

    Add Student class with properties and data annotations

---
+++ StudentCodeFirst/Student.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentCodeFirst
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
